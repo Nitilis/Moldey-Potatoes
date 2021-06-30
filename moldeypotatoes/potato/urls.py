eaeb02e2f68b1ba87afc86b86e7bec1910ae54b0
Ryan Reynolds <lycoes@gmail.com>
2021-06-30 14:54:08 -0400

    Update urls.py

--- moldeypotatoes/potato/urls.py
+++ moldeypotatoes/potato/urls.py
@@ -5,7 +5,6 @@ from django.conf import settings
 urlpatterns = [
     path('', views.splash, name='splash'),
     path('loginreg', views.loginreg, name='loginreg'),
-    path('infopage/', views.infopage, name='infopage'),
     path('profile', views.profile, name='profile'),
     path('create_user', views.create_user, name='create_user'),
     path('login', views.login, name='login'),
@@ -13,7 +12,6 @@ urlpatterns = [
     path('all_games', views.all_games, name='games'), 
     path('all_books', views.all_books, name='games'),
     path('all_movies', views.all_movies, name='games'),
-
     path('book_info', views.book_info, name='book_info'),
     path('game_info', views.game_info, name='game_info'),
     path('movie_info', views.movie_info, name='movie_info'),
