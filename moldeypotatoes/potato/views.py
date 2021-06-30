394140e3d9b1abff983923194840472181046a79
Ryan Reynolds <lycoes@gmail.com>
2021-06-30 14:49:17 -0400

    Added more to db
    More to DB,  minor edits to others

--- moldeypotatoes/potato/views.py
+++ moldeypotatoes/potato/views.py
@@ -6,11 +6,6 @@ import bcrypt
 def loginreg(request):
     return render(request, 'loginreg.html')
 
-def infopage(request):
-    if "user_id" not in request.session:
-        return redirect('/')
-    return render(request, 'infopage.html')
-
 def profile(request):
     if "user_id" not in request.session:
         return redirect('/')
@@ -76,7 +71,26 @@ def all_movies(request):
     return render(request, 'movies.html', context)
 
 def movie_info(request):
+    if "user_id" not in request.session:
+        return redirect('/')
     context = {
         'movie' : Movie.objects.get(),
     }
     return render(request, 'movie_info.html', context)
+
+def game_info(request):
+    if "user_id" not in request.session:
+        return redirect('/')
+    context = {
+        'game': Game.objects.get(),
+    }
+    return render(request, 'infopage.html', context)
+
+
+def book_info(request):
+    if "user_id" not in request.session:
+        return redirect('/')
+    context = {
+        'book': Book.objects.get(),
+    }
+    return render(request, 'infopage.html', context)
